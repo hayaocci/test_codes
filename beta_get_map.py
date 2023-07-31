7aeb91c4fcbd1f2bf51760c0a47c26e4d1a9f946
Shun Taguchi <107763324+hayaocci@users.noreply.github.com>
2023-08-01 03:13:32 +0900

    Update beta_get_map.py

--- beta_get_map.py
+++ beta_get_map.py
@@ -1,19 +1,46 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import csv
+import math
 
 def get_map(file_path):
     with open(file_path) as f:
         #スペースで区切る
-        reader = csv.reader(f, delimiter=' ')
+        reader = csv.reader(f, delimiter=',')
         data = [row for row in reader]
 
+        data_2 = [list(x) for x in zip(*data)]
+        data_2_lat = [float(v) for v in data_2[0]]
+        data_2_lon = [float(v) for v in data_2[1]]
+        data_2_azimuth = [float(v) for v in data_2[2]]
+
+        cos_azimuth_array = []
+        sin_azimuth_array = []
+        for i in range(8):
+            cos_azimuth = math.cos(data_2_azimuth[i])
+            sin_azimuth = math.sin(data_2_azimuth[i])
+            cos_azimuth_array.append(cos_azimuth)
+            sin_azimuth_array.append(sin_azimuth)
+        
+        plt.barbs(data_2_lat, data_2_lon, cos_azimuth_array, sin_azimuth_array, length=5, pivot='middle')
+        plt.show()
+
+        print(data_2_lat)
+        print(data_2_lon)
+        print(data_2_azimuth)
+
+
+        plt.scatter(data_2_lat, data_2_lon) #, s=10, c='blue', marker='o', label='rover')
+        plt.show()
+
     #-----lat, lonの取得-----#
-    for _ in range(5):
-        lat = data[0][3]
-        lon = data[0][4]
-        rover_azimuth = data[0][5]
-        print(lat, lon, rover_azimuth)
+    for i in range(5):
+        lat = data[i][0]
+        lon = data[i][1]
+        rover_azimuth = data[i][2]
+        # print(lat, lon, rover_azimuth)
+        # print(data_2[0])
+
 
 if __name__ == '__main__':
-    file_path = '/home/kei/document/experiments/2020_12_10_13_40_09/sequence/2020_12_10_13_40_09.csv'
+    file_path = 'test_data.csv'
     get_map(file_path)
