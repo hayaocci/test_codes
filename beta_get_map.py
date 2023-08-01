4c86ad6a2c78456ac80f20746e283d47a2fe115a
hayaocci <7319079@ed.tus.ac.jp>
2023-08-01 12:02:56 +0900

    a

--- beta_get_map.py
+++ beta_get_map.py
@@ -8,12 +8,15 @@ def get_map(file_path, lat_goal, lon_goal):
     with open(file_path) as f:
         #スペースで区切る
         reader = csv.reader(f, delimiter=',')
-        data = [row for row in reader]
+        log_data = [row for row in reader]
 
-        data_2 = [list(x) for x in zip(*data)]
-        data_2_lat = [float(v) for v in data_2[0]]
-        data_2_lon = [float(v) for v in data_2[1]]
-        rover_azimuth = [float(v) for v in data_2[2]]
+        #転地行列の取得
+        log_data_T = [list(x) for x in zip(*log_data)]
+
+        #-----緯度・経度・方位角の取得-----#
+        gps_lat = [float(v) for v in log_data_T[0]]
+        gps_lon = [float(v) for v in log_data_T[1]]
+        rover_azimuth = [float(v) for v in log_data_T[2]]
 
         cos_azimuth_array = []
         sin_azimuth_array = []
@@ -32,11 +35,11 @@ def get_map(file_path, lat_goal, lon_goal):
         #グラフのタイトル
         plt.title("A visualize control record")
 
-        #軸の範囲
-        lim_x1 = min(data_2_lat) - 0.000010
-        lim_x2 = max(data_2_lat) + 0.000010
-        lim_y1 = min(data_2_lon) - 0.000010
-        lim_y2 = max(data_2_lon) + 0.000010
+        #軸の範囲(少し大き目に設定)
+        lim_x1 = min(gps_lat) - 0.000010
+        lim_x2 = max(gps_lat) + 0.000010
+        lim_y1 = min(gps_lon) - 0.000010
+        lim_y2 = max(gps_lon) + 0.000010
         plt.xlim(lim_x1, lim_x2)
         plt.ylim(lim_y1, lim_y2)
 
@@ -49,14 +52,14 @@ def get_map(file_path, lat_goal, lon_goal):
         plt.ylabel("Longitude")
 
         #座標のプロット
-        plt.scatter(data_2_lat[0], data_2_lon[0], color="blue")
-        plt.quiver(data_2_lat, data_2_lon, cos_azimuth_array, sin_azimuth_array, color="red")
-        plt.plot(data_2_lat, data_2_lon, label="Trajectory", linestyle="dashed", color="black")
+        plt.scatter(gps_lat[0], gps_lon[0], color="blue")
+        plt.quiver(gps_lat, gps_lon, cos_azimuth_array, sin_azimuth_array, color="red")
+        plt.plot(gps_lat, gps_lon, label="Trajectory", linestyle="dashed", color="black")
         
         #スタート地点、ゴール地点、制御終了地点の座標の表示
-        plt.scatter(data_2_lat[0], data_2_lon[0], color="blue", edgecolors="black")
+        plt.scatter(gps_lat[0], gps_lon[0], color="blue", edgecolors="black")
         plt.scatter(lat_goal, lon_goal, color="red", edgecolors="black")
-        plt.scatter(data_2_lat[-1], data_2_lon[-1], color="red", edgecolors="black")
+        plt.scatter(gps_lat[-1], gps_lon[-1], color="red", edgecolors="black")
 
 
 
@@ -64,8 +67,8 @@ def get_map(file_path, lat_goal, lon_goal):
         arrow_dict = dict(arrowstyle="simple", color="gray", connectionstyle="arc3")
         text_dict = text_dict = dict(boxstyle="round",fc="silver", ec="mediumblue")
         plt.annotate("Target point" + "\n" + str(lat_goal) + ", "+ str(lon_goal) + ", "+ "altitude", xy=(lat_goal, lon_goal), xytext=(lat_goal - 0.000025, lon_goal - 0.000020), arrowprops=arrow_dict, bbox=text_dict)
-        plt.annotate("Control start point" + "\n" + str(data_2_lat[0]) + ", "+ str(data_2_lon[0]) + ", "+ "altitude", xy=(data_2_lat[0], data_2_lon[0]), xytext=(data_2_lat[0] + 0.000005, data_2_lon[0] - 0.000020), arrowprops=arrow_dict, bbox=text_dict)
-        plt.annotate("Control finish point" + "\n" + str(data_2_lat[-1]) + ", "+ str(data_2_lon[-1]) + ", "+ "altitude", xy=(data_2_lat[-1], data_2_lon[-1]), xytext=(data_2_lat[-1] - 0.000020, data_2_lon[-1] + 0.000009), arrowprops=arrow_dict, bbox=text_dict)
+        plt.annotate("Control start point" + "\n" + str(gps_lat[0]) + ", "+ str(gps_lon[0]) + ", "+ "altitude", xy=(gps_lat[0], gps_lon[0]), xytext=(gps_lat[0] + 0.000005, gps_lon[0] - 0.000020), arrowprops=arrow_dict, bbox=text_dict)
+        plt.annotate("Control finish point" + "\n" + str(gps_lat[-1]) + ", "+ str(gps_lon[-1]) + ", "+ "altitude", xy=(gps_lat[-1], gps_lon[-1]), xytext=(gps_lat[-1] - 0.000020, gps_lon[-1] + 0.000009), arrowprops=arrow_dict, bbox=text_dict)
         
         #グリッドの表示
         plt.grid()
@@ -77,21 +80,21 @@ def get_map(file_path, lat_goal, lon_goal):
         plt.savefig('A visual control record.png', bbox_inches='tight')
         plt.show()
 
-        print(data_2_lat)
-        print(data_2_lon)
+        print(gps_lat)
+        print(gps_lon)
         print(rover_azimuth)
 
 
-        # plt.scatter(data_2_lat, data_2_lon) #, s=10, c='blue', marker='o', label='rover')
+        # plt.scatter(gps_lat, gps_lon) #, s=10, c='blue', marker='o', label='rover')
         # plt.show()
 
     #-----lat, lonの取得-----#
     # for i in range(5):
-    #     lat = data[i][0]
-    #     lon = data[i][1]
-    #     rover_azimuth = data[i][2]
+    #     lat = log_data[i][0]
+    #     lon = log_data[i][1]
+    #     rover_azimuth = log_data[i][2]
     #     # print(lat, lon, rover_azimuth)
-    #     # print(data_2[0])
+    #     # print(log_data_T[0])
 
 
 if __name__ == '__main__':
