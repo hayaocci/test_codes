1636c72f560436f72c7ac74a2c934c7c4a31d2fc
Shun Taguchi <107763324+hayaocci@users.noreply.github.com>
2023-09-02 08:21:30 +0900

    a

--- a__.py
+++ a__.py
@@ -1,8 +1,13 @@
-import copy
+# import copy
 
-a = [1,2 ]
-b = copy.copy(a)
-print(a, b)
+# a = [1,2 ]
+# b = copy.copy(a)
+# print(a, b)
 
-a[0] = 100
-print(a, b)
+# a[0] = 100
+# print(a, b)
+
+while 1:
+    a = int(input('input:'))
+
+    print(a%10)
