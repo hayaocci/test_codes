c0d645990d31235309275eb128be169ec1a38d15
hayaocci <7319079@ed.tus.ac.jp>
2023-10-23 20:33:01 +0900

    a

--- sony_dice/save_img.py
+++ sony_dice/save_img.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 
 # .npyファイルのパスを指定
-file_path = "X_test.npy"  # 実際の.npyファイルのパスに置き換えてください
+file_path = "X_train.npy"  # 実際の.npyファイルのパスに置き換えてください
 
 # .npyファイルを読み込む
 data = np.load(file_path)
@@ -15,7 +15,7 @@ num_images = data.shape[0]  # 画像の枚数
 for i in range(num_images):
     image_data = data[i].reshape(image_size, image_size)
     image = Image.fromarray(image_data.astype('uint8'))  # 変換不要
-    image_path = f"test/{i}.png"  # 保存先のフォルダを指定
+    image_path = f"train/{i}.png"  # 保存先のフォルダを指定
     image.save(image_path)
 
 print(f"{num_images} 画像をJPEGファイルに test フォルダに保存しました。")
